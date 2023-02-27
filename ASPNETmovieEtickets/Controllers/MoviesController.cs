c914390c7887d29752bd9d20c0f3539c88fcbfdc
sone <sthaya@gmail.com>
2023-02-26 20:19:55 -0800

    created controllers for Movies, Actors, Producers, Cinemas

---
+++ ASPNETmovieEtickets/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using ASPNETmovieEtickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETmovieEtickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
