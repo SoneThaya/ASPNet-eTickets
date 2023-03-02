b5f65d5126d87c399fa770caaceac502880be30f
sone <sthaya@gmail.com>
2023-03-01 21:41:50 -0800

    producers view

--- ASPNETmovieEtickets/Controllers/ProducersController.cs
+++ ASPNETmovieEtickets/Controllers/ProducersController.cs
@@ -15,7 +15,7 @@ namespace ASPNETmovieEtickets.Controllers
         public async Task<IActionResult> Index()
         {
             var allProducers = await _context.Producers.ToListAsync();
-            return View();
+            return View(allProducers);
         }
     }
 }
--- ASPNETmovieEtickets/Models/Producer.cs
+++ ASPNETmovieEtickets/Models/Producer.cs
@@ -6,8 +6,12 @@ namespace ASPNETmovieEtickets.Models
     {
         [Key]
         public int Id { get; set; }
+
         public string ProfilePictureURL { get; set; }
+     
         public string FullName { get; set; }
+      
+
         public string Bio { get; set; }
 
         // Relationships
